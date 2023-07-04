4dc1cbeabacec2a9eab9215be1dc0e776f137542
Nico Garcia <nicolasgarcia9812@hotmail.com>
2023-07-04 02:54:38 -0300

    BD hecha, Rutas y Controllers empezado

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,38 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define("dog", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    imagen: {
+      type: DataTypes.BLOB,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    altura: {
+      type: DataTypes.FLOAT,
+      validate: {
+        min: 0,
+      },
+    },
+    peso: {
+      type: DataTypes.FLOAT,
+      validate: {
+        min: 0,
+      },
+    },
+    anios: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 0,
+      },
     },
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,20 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const {
+  getDogs,
+  getUniqueDog,
+  postDog,
+} = require("../controllers/dogsController");
+const { getTemperaments } = require("../controllers/temperamentsController");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.get("/dogs", getDogs);
+router.get("/dogs/:idRaza", getUniqueDog);
+router.post("/dogs", postDog);
+router.get("/temperaments", getTemperaments);
 
 module.exports = router;
