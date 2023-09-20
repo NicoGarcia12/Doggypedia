28fb5eca1d4811eed6232d53cdadf8d1b6f8cad8
Nico Garcia <nicolasgarcia9812@hotmail.com>
2023-09-20 14:29:07 -0300

    Load dogs

--- client/src/views/Home/home.jsx
+++ client/src/views/Home/home.jsx
@@ -1,7 +1,15 @@
 import style from "./home.module.css";
 import Dogs from "../../components/Dogs/dogs";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { peticionDogs } from "../../redux/actions";
 
 export default function Home() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(peticionDogs());
+  }, [dispatch]);
   return (
     <div className={style.homeContainer}>
       <Dogs />
