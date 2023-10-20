8949c2752aac9d26c4e1b83b57871e432c19bb60
Nico Garcia <nicolasgarcia9812@hotmail.com>
2023-10-20 18:48:51 -0300

    Cambios, prueba

--- api/src/handlers/Dogs/getUniqueDogHandler.js
+++ api/src/handlers/Dogs/getUniqueDogHandler.js
@@ -14,7 +14,7 @@ const getUniqueDogHandler = async (req, res) => {
     }
     return res.status(200).json(response);
   } catch (error) {
-    if (error.message === "Dog not found") {
+    if (error.message === "Dogsasa not found") {
       return res.status(404).json(error.message);
     } else {
       return res.status(500).json(error.message);
